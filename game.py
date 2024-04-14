c5cf794eb73dcfcb907223d8a78ac50d155cc818
Marcus Schwarz <marcus.w.schwarz@gmail.com>
2024-04-13 20:45:14 -0400

    Committing the walking animation!

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import sprites
 
 pygame.init()
 
@@ -11,24 +12,38 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Fallen Apart")
 
 #Player
-player_start_x = 200
-player_start_y = 200
-base_sprite = pygame.image.load("img/fixed_base_sprite_facing_screen.png")
-#Downscale Image 
-player_sprite = pygame.transform.scale(base_sprite, (PLAYER_WIDTH, PLAYER_HEIGHT))
-#Creates a hitbox
-player_hitbox = player_sprite.get_rect()
-#Spawn location
-player_hitbox.center = (player_start_x, player_start_y)
+player = sprites.Player(screen, 500,500, 45, 60)
+# player_start_x = 200
+# player_start_y = 200
+
+#player.test()
+
+clock = pygame.time.Clock()
 
 
 run = True
 while run:
+    dt = clock.tick(60)
     screen.fill("blue")
-    screen.blit(player_sprite,player_hitbox)
+    player.update(dt)
 
     #Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.player_ani.direction(0)
+                player.movex = -1 * player.speed
+            if event.key == pygame.K_d:
+                player.player_ani.direction(1)
+                player.movex = 1 * player.speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                player.movex = 0
+            if event.key == pygame.K_d:
+                player.movex = 0
+
+
     pygame.display.update()
